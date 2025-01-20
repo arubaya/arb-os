244073de96863eaac6c564df348db4f5b701c9b4
arubaya <abi.arubaya@gmail.com>
2025-01-20 19:28:05 +0700

    Setup base project

---
+++ apps/main-shell/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "ArbOS",
+  description:
+    "Clone the iconic looks and vibes of Windows, macOS, or Linux. Launch your experience now!",
+  icons: ["/logo-icon.png"],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ apps/main-shell/components/shared/BackgroundAnimation/BackgroundAnimation.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./backgroundAnimation.module.css";
+
+const BackgroundAnimation = () => {
+  return (
+    <div className={styles.background}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default BackgroundAnimation;
